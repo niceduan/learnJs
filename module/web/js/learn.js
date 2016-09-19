3454381a0aa25de96e1c2a5414046b7a9993f9bf
dgx <niceduan@outlook.com>
2016-09-19 14:50:58 +0800

    写了那么久了进行适当的提交

--- module/web/js/learn.js
+++ module/web/js/learn.js
@@ -119,7 +119,7 @@ window.onerror = function (msg,url,line) {
         alert("ERROR:" + msg + "\n" + url + ":" + line);
         return true;
     }
-}
+};
 onerror.max = 3;
 onerror.num = 3;
 //显示的声明会隐藏隐式的变量元素
@@ -146,6 +146,82 @@ ww.close();Window.close();
 * parent 表示上级窗口
 * top 表示顶级窗口
 * */
+var iframeElement = document.getElementById("f1");
+var childFrame = document.getElementById("f1").contentWindow;
+var elt = document.getElementById("f1");
+var win = elt.contentWindow;
+win.frameElement === elt //对于帧来说永远只true
+window.frameElement === null //对于顶级窗口来说永远是true
+/*
+* instanceof不能跨窗口工作,每个窗口都有自己原型对象
+*
+* */
+function getElements(/*ids*/){
+    var elements = {};
+    arguments.forEach(id=>{
+        var ele = document.getElementById(arguments[id]);
+        if(ele==null)
+            throw new Error("NO element with id:" +arguments[id]);
+        elements[arguments[id]] = ele;
+    });
+    return elements;
+}
+var spans = document.getElementsByTagName("span");
+var firstPara = document.getElementsByTagName("p")[0];
+var firstParaSpans = firstPara.getElementsByTagName("span");
+document.images.map(image=>image.style.display ="none");
+/*
+* 不能在NodeList和HTML集合上调用Array的方法，那就间接的使用
+* */
+var content = Array.prototype.map.call(document.getElementsByTagName("p"),function (e) {
+    return e.innerHTML;
+});
+//他们的item方法 @param int @return int (index)
+//naemdItem方法的话，返回 属性名的值
+//这两个玩意是实时更新的，所以需要在必要的时候生成静态的副本
+var snapshot = Array.prototype.slice.call(nodelist, 0);
+
+/*
+* 通过CSS选取元素
+* 高端的空格隔开法....
+*
+* */
+var warnings = document.getElementsByClassName("warning");
+var log = document.getElementById("log");
+var fatal = log.getElementsByClassName("fatal error");
+
+/*
+* CSS               选择器
+* #nav              id=nav 的元素
+* div               所有的<div>元素
+* .warning          所有的class属性中包含了“warning”的元素
+* p[lang="fr"]      所有的法语段落，如<p lang="fr">
+* *[name="x"]       所有的包含name = “x” 属性的元素
+* span.fatal.error  <span>元素，其class中包含fatal和error
+* span[lang="fr"].warning      所有的使用法语的且且class中包含warning的<span>
+*
+    * 选择器指示文档的结构
+* #log span          id= “log”的元素的后代元素中的所有的<span>元素
+* #log>span          id= “log”的元素的子元素中的所有的<span>元素
+* body>h1:first-child       <body>的子元素的第一个<h1>元素
+*
+* */
+//div ， #log 并集组合
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
